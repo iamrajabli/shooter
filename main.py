90d810f5009ad56be8d837ba30be9224431dbe5b
iamrajabli <iamrajabli@outlook.com>
2023-04-10 12:39:33 +0400

    added logic moving rocket

--- main.py
+++ main.py
@@ -7,20 +7,35 @@ pygame.init()
 pygame.display.set_caption("Awesome Shooter Game")
 
 screen_width, screen_height = 800, 600
-
 screen = pygame.display.set_mode((screen_width, screen_height))
 screen_fill_color = (32, 52, 71)
 
 fighter_image = pygame.image.load("images/fighter.png")
-
 fighter_width, fighter_height = fighter_image.get_size()
-
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
+fighter_is_moving_left, fighter_is_moving_right = False, False
+FIGHTER_STEP = 1
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = True
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = False
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = False
+
+    if fighter_is_moving_left and fighter_x >= FIGHTER_STEP:
+        fighter_x -= FIGHTER_STEP
+
+    if fighter_is_moving_right and screen_width - fighter_width >= fighter_x + FIGHTER_STEP:
+        fighter_x += FIGHTER_STEP
 
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
